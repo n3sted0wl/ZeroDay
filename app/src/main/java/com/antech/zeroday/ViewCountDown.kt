f5d667f5673926a631df49ccb9d5dcb3a154114f
Paul Antonio <paul.antonio2017@gmail.com>
2018-11-30 23:41:28 -0500

    documentation
    documenting the initial page

--- app/src/main/java/com/antech/zeroday/ViewCountDown.kt
+++ app/src/main/java/com/antech/zeroday/ViewCountDown.kt
@@ -1,12 +1,15 @@
 package com.antech.zeroday
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
 class ViewCountDown : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_view_count_down)
+        setContentView(R.layout.activity_view_count_down) // gets the activity_view_count_down xml file
     }
+
+    /*
+     * In tools > sdk manager, check out which packages you have
+     */
 }
