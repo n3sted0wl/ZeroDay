8e25ad8093bc39bc1b6cbe0bc65cf565004a84ea
Paul Antonio <paul.antonio2017@gmail.com>
2018-12-01 01:55:00 -0500

    ReceiveMessage activity

---
+++ app/src/main/java/com/antech/zeroday/ReceivingMessage.kt
@@ -0,0 +1,12 @@
+package com.antech.zeroday
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ReceivingMessage : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_receiving_message)
+    }
+}
