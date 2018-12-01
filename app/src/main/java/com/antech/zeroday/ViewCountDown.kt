affa8ed291bf77c6464438482479ac337062f061
Paul Antonio <paul.antonio2017@gmail.com>
2018-11-30 20:13:32 -0500

    second init
    idk what else this involves... seems like a lot of boilerplate. which is what kotlin is supposed to rid of

---
+++ app/src/main/java/com/antech/zeroday/ViewCountDown.kt
@@ -0,0 +1,12 @@
+package com.antech.zeroday
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewCountDown : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_count_down)
+    }
+}
